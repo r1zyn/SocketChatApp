33db31907910d0a64ce1738de8d19de9273cd5ab
Ascendus <ascendus.dev@gmail.com>
2022-08-16 22:55:41 +1200

    feat: updated links

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const io = require("socket.io")(3000, {
     cors: {
-        origin: "https://socketchat-app.vercel.app/",
+        origin: "https://socketchat-app.vercel.app",
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
     }
 });
