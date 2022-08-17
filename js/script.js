e6f46d7e977f0f7cc1d720e3f941e89e1338eece
Ascendus <ascendus.dev@gmail.com>
2022-08-17 13:56:28 +1200

    fix: added default username

--- js/script.js
+++ js/script.js
@@ -5,9 +5,8 @@ const messageInput = document.getElementById("message-input");
 const fileUploader = document.getElementById("file-uploader");
 
 const _name = prompt("What is your name? (If blank, name will be displayed as \"Anonymous User\")", "Anonymous User");
-if (_name == null) close();
 appendMessage(`[${new Date().toLocaleTimeString()}] You joined`);
-socket.emit("new-user", _name);
+socket.emit("new-user", _name || "Anonymous User");
 
 onAppend(messageContainer, () => {
     window.scrollTo(0, document.body.scrollHeight);
