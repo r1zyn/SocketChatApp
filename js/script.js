5586024d85265a86f35cfd361fc45c588c4fb5e3
Ascendus <ascendus.dev@gmail.com>
2022-08-17 22:28:52 +1200

    feat: added changes

--- js/script.js
+++ js/script.js
@@ -9,16 +9,18 @@ const passwordInput = document.getElementById("password-input");
 
 let authenticated = false;
 
-loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    if (passwordInput.value !== "ym gets bitches") { // Doesn't matter if people find this anyways, and ignore the password
-        alert("Invalid password provided");
-    } else {
-        authenticated = true;
-        chatContainer.style.display = "block";
-        loginForm.style.display = "none";
-    }
+document.addEventListener("load", () => {
+    loginForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+    
+        if (passwordInput.value !== "ym gets bitches") { // Doesn't matter if people find this anyways, and ignore the password
+            alert("Invalid password provided");
+        } else {
+            authenticated = true;
+            chatContainer.style.display = "block";
+            loginForm.style.display = "none";
+        }
+    });
 });
 
 if (authenticated) {
