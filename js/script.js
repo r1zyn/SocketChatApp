5cb3daa18d3dfabb275300045000a23e9e52ea0c
Ascendus <ascendus.dev@gmail.com>
2022-08-17 13:48:18 +1200

    feat: minor changes

--- js/script.js
+++ js/script.js
@@ -46,6 +46,8 @@ fileUploader.addEventListener("change", e => {
             type: "image"
         };
     } else {
+        const uploadContainer = document.createElement("div");
+        uploadContainer.style.width = "100%";
         const uploadFile = document.createElement("a");
         const icon = document.createElement("svg");
         icon.innerHTML = '<svg aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M16.293 9.293L17.707 10.707L12 16.414L6.29297 10.707L7.70697 9.293L11 12.586V2H13V12.586L16.293 9.293ZM18 20V18H20V20C20 21.102 19.104 22 18 22H6C4.896 22 4 21.102 4 20V18H6V20H18Z"></path></svg>';
@@ -71,9 +73,10 @@ fileUploader.addEventListener("change", e => {
         innerText.innerText = file.name;
         uploadFile.download = file.name.replaceAll(/(\.[a-z]{3,})$/g, "");
         uploadFile.href = window.URL.createObjectURL(file);
+        uploadContainer.appendChild(uploadFile);
 
         fileToUpload = {
-            file: uploadFile,
+            file: uploadContainer,
             type: "file"
         }
     }
